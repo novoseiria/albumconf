b33bed425f6683c58d4cfd6c562025d6fdaf7bf0
Nile Jocson <novoseiria@gmail.com>
2026-05-29 23:18:12 +0800

    Outcome enums should be public

--- src/outcome.rs
+++ src/outcome.rs
@@ -1,24 +1,30 @@
 // SPDX-FileCopyrightText: Copyright (C) Nile Jocson <novoseiria@gmail.com>
 // SPDX-License-Identifier: MPL-2.0
 
+use std::path::PathBuf;
+
 use thiserror::Error;
 
 
 
 #[derive(Error, Debug)]
-enum Exit
+pub enum Exit
+{
+	#[error("{path} is not a directory")]
+	NotADirectory
+	{
+		path: PathBuf
+	}
+}
+
+#[derive(Error, Debug)]
+pub enum Fatal
 {
 
 }
 
 #[derive(Error, Debug)]
-enum Fatal
-{
-
-}
-
-#[derive(Error, Debug)]
-enum Outcome
+pub enum Outcome
 {
 	#[error(transparent)]
 	Exit(#[from] Exit),
